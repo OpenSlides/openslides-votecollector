4883de2631ba0429da9db47eb054da3bc8438a34
Emanuel Schütze <emanuel@intevation.de>
2017-06-11 22:08:18 +0200

    Release 2.0.2

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
